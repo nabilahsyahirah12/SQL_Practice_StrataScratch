a9d788322442437f1af14e62806a27660629ac49
nabilahsyahirah12 <nabilahsyahirah@hotmail.com>
2024-12-26 22:12:22 +0800

    10299_Finding_Updated_Records.sql

--- Easy/10299_Finding_Updated_Records.sql
+++ Easy/10299_Finding_Updated_Records.sql
@@ -1,3 +1,5 @@
+--https://platform.stratascratch.com/coding/10299-finding-updated-records?code_type=1
+
 select id, first_name, last_name, department_id, max(salary) AS current salary
 from ms_employee_salary
 group by id, first_name, last_name, department_id
